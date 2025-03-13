7ec4c999b5077f1c2ca13a418ae748ea03976a86
Nathan Kamenchu <kamenchunathan@gmail.com>
2025-03-13 23:05:33 +0300

    init: Fetch titled tuesday results and append results to a dataframe

---
+++ drawn_out/test/main.py
@@ -0,0 +1,33 @@
+sample_pgn = """
+[Event "Testing testing"]
+[Site "Local"]
+[Date "2025.03.05"]
+[Round "?"]
+[White "Cheese98"]
+[Black "blue867"]
+[Result "1/2-1/2"]
+[WhiteElo "892"]
+[BlackElo "1004"]
+[Orientation "white"]
+
+1. e4 e5 2. Nc3 Nf6 3. f4 d5 4. fxe5 Nxe4 5. Nf3 Bc5 6. d4 Bb4 7. Bd2 Bg4 8. Nxe4 dxe4 9. Bxb4 exf3 10. Qd2 Nc6 11. Bb5 Qd5 12. Bxc6+ Qxc6 13. O-O O-O-O 14. Rf2 Qd5 15. h3 Be6 16. Rd1 fxg2 17. b3 Qe4 18. Rf4 Qg6 19. Qxg2 Qxg2+ 20. Kxg2 b6 21. Rff1 a5 22. Ba3 b5 23. c3 a4 24. bxa4 bxa4 25. Rd2 h5 26. Kh2 Rhe8 27. h4 Bc4 28. Rf5 f6 29. exf6 gxf6 30. Rxh5 Re1 31. Rf2 Rg8 32. Bb4 Bf7 33. Rhf5 Rg4 34. Ba5 Rxh4+ 35. Kg3 Rh7 36. Rc5 Re3+ 37. Kf4 Re7 38. d5 Rh4+ 39. Kg3 Rh5 40. Rxf6 Bxd5 41. c4 Rg7+ 42. Kf2 Rh2+ 43. Ke3 Bh1 44. Kd4 Rxa2 45. Rf8+ Kb7 46. Rb5+ Ka6 47. Be1 Re2 48. Bf2 a3 49. Kd3 Rb2 50. Rxb2 axb2 51. Kc2 Rg4 52. Rh8 Rxc4+ 53. Kxb2 Be4 54. Bg3 Kb5 55. Bxc7 Rxc7 56. Rh5+ Kb4 57. Rh2 Rg7 58. Re2 Bf5 59. Rf2 Bd3 60. Rf4+ Bc4 61. Rf2 Ra7 62. Rf3 Ra2+ 63. Kc1 Kc5 64. Rf8 Kd4 65. Kd1 Rg2 66. Rf4+ Ke3 67. Rh4 Bb3+ 68. Kc1 Rc2+ 69. Kb1 Rf2 70. Rh3+ Ke4 71. Rxb3 Rf1+ 72. Kc2 Rf4 73. Kd1 Rf1+ 74. Kc2 Rf2+ 75. Kd1 Rf1+ 76. Kc2 Rf2+ 77. Kd1 Rf1+ 78. Kc2 Rf2+ 79. Kd1 Rf1+ 80. Kc2 Rf2+ 81. Kd1 1/2-1/2
+"""
+
+
+sample_pgn_2 = """
+[Event "Live Chess"]
+[Site "Chess.com"]
+[Date "2025.03.06"]
+[Round "-"]
+[White "hesandewsitha"]
+[Black "nae_dawg"]
+[Result "1-0"]
+[WhiteElo "386"]
+[BlackElo "357"]
+[TimeControl "180"]
+[ECO "B23"]
+
+1. e4 c5 2. Nc3 Nc6 3. d3 d6 4. Bg5 Nf6 5. Nf3 h6 6. Bxf6 exf6 7. Be2 Ne7 8. O-O g6 9. Qd2 Bg7 10. Nb5 O-O 11. Qf4 d5 12. exd5 Nxd5 13. Qc4 b6 14. d4 Be6 15. dxc5 Ne3 16. Qd3 Nxf1 17. Rxf1 bxc5 18. Qa3 a6 19. Rd1 Qb6 20. Nd6 f5 21. Nc4 Qc7 22. Ne3 c4 23. Nxc4 Bxc4 24. Bxc4 Qxc4 25. c3 Qb5 26. b4 Rac8 27. Rc1 Rfe8 28. Nd4 Bxd4 29. cxd4 Rxc1+ 30. Qxc1 Qxb4 31. a3 Qxd4 32. h3 1-0
+"""
+
+
