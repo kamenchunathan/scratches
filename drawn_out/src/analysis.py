ebf77b11bab42244b44a983537443dc4b8e4a73d
Nathan Kamenchu <kamenchunathan@gmail.com>
2025-03-17 15:44:48 +0300

    feat: Add analysis code to find rook endgames

--- drawn_out/src/analysis.py
+++ drawn_out/src/analysis.py
@@ -1,11 +1,24 @@
-def drawn_out_game_info(pgn: str):
+import io
+import os
+import re
+from typing import Optional
+
+import chess.pgn
+import libsql_experimental as libsql
+import pandas as pd
+
+
+ROOK_ENDGAME_PIECES = ['r', 'k', 'K', 'R']
+
+
+def analyze_game(pgn: str):
     game = chess.pgn.read_game(io.StringIO(pgn))
         
-    end_node = game.end()
-    board = end_node.board()
-
-    if not is_rook_endgame(board.board_fen()):
-        return {}
+    board = game.end().board()
+    
+    (is_rook_endgame, moves_played_out) = moves_played_if_is_rook_endgame(board)
+    if not is_rook_endgame:
+        return
     
     outcome = board.outcome(claim_draw=True)
     outcome_str = None
@@ -23,27 +36,93 @@ def drawn_out_game_info(pgn: str):
                 winner = 'w'
             case '0-1':
                 winner = 'b'
-                
-        
-    moves_played_out = endgame_moves_played(board.copy())
-    return  {
-        'rook_endgame': is_rook_endgame(board.board_fen()),
-        'draw_reason': outcome_str, 
-        'winner': winner,
-        'played_out': moves_played_out
+    
+    return {
+        "rook_endgame": True,
+        "draw_reason": outcome_str,
+        "winner": winner,
+        "played_out": moves_played_out
     }
 
 
-def endgame_moves_played(board: chess.Board) -> int:
+def moves_played_if_is_rook_endgame(board: chess.Board) -> bool:
+    def check_is_rook_endgame(board_fen: str) -> bool:
+        """Checks whether remaining pieces are either a King or a rook"""
+        return all(p in ROOK_ENDGAME_PIECES for p in re.findall(r'[a-zA-Z]', board_fen))
+    
     moves = 0
-    while is_rook_endgame(board.board_fen()):
+    is_rook_endgame = False
+    last_move = None
+    while True:
+        if not check_is_rook_endgame(board.board_fen()):
+            # First iteration is false meaning it's not a rook endgame
+            if moves == 0:
+                is_rook_endgame = False
+            else:
+                # The 
+                # Check that the next board position has 4 pieces: King and rook of both colors
+                board.push(last_move)
+                is_rook_endgame =  \
+                    ( sum(board.pieces(chess.ROOK, chess.WHITE).tolist()) \
+                        + sum(board.pieces(chess.ROOK, chess.BLACK).tolist()) \
+                        + sum(board.pieces(chess.KING, chess.WHITE).tolist()) \
+                        + sum(board.pieces(chess.KING, chess.BLACK).tolist()) \
+                    ) == 4
+            break
+        
         moves +=1
-        board.pop()
-    return moves
-
-
-def is_rook_endgame(board_fen: str) -> bool:
-    return all(p in rook_endgame_pieces for p in re.findall(r'[a-zA-Z]', board_fen))
+        last_move = board.pop()
+    
+    return is_rook_endgame, moves
     
 
+def analyze_data(df: pd.DataFrame):
+    analyzed_df = df['pgn'].apply(lambda row: pd.Series(analyze_game(row))),
+    if analyzed_df[0].empty:
+        return pd.DataFrame(
+            columns= [ "rook_endgame", "draw_reason", "winner", "played_out" ]
+        )
+    return df.join(analyzed_df[0]).dropna(
+        how="all", 
+        subset=["rook_endgame", "draw_reason", "winner", "played_out"]
+    )
+    print(res)
 
+
+def fetch_data(db_uri: str, db_token: str, tournament_id):
+    db = libsql.connect(db_uri, auth_token=db_token)
+    return db.execute(
+        """SELECT id, round, pgn, tournament_id FROM game WHERE tournament_id = ? AND status = 'success';""",
+        (tournament_id,)
+    ).fetchall()
+
+
+def main():
+    DB_URI = os.getenv("DB_URI")
+    DB_TOKEN = os.getenv("DB_TOKEN")
+    
+    if DB_URI is None or DB_TOKEN is None:
+        print('Required environement variables are set in the environment')
+        return
+    
+    db = libsql.connect(DB_URI, auth_token=DB_TOKEN)
+    completed_tournaments = db.execute(
+        "SELECT tournament_id FROM jobs WHERE status = 'complete';"
+    ).fetchall()
+
+    total = pd.concat(
+        analyze_data(
+            pd.DataFrame(
+                fetch_data(DB_URI, DB_TOKEN, tournament_id[0]), 
+                columns=["id", "round", "pgn", "tournament_id"]
+            ).set_index("id")
+        )
+        for tournament_id in completed_tournaments
+    )
+    
+    with open('data/analyzed.csv', 'w') as f:
+        total.to_csv(f)
+ 
+
+if __name__ == '__main__':
+    main()
