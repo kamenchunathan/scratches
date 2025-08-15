eb1b5f5708384293abf2c2179e88be20a7702ed4
Live session user <info@kamenchunathan.com>
2025-08-15 10:48:28 +0000

    refactor: Edit to return a token struct

--- brainfuck/src/tokenizer.rs
+++ brainfuck/src/tokenizer.rs
@@ -1,9 +1,9 @@
 use std::iter::Enumerate;
 use std::str::Chars;
 
-use crate::TokenType;
+use crate::{Token, TokenType};
 
-fn tokenize(input: &str) -> Vec<TokenType> {
+pub fn tokenize(input: &str) -> Vec<Token<()>> {
     let mut tokenizer = Tokenizer::new(input);
     tokenizer.tokenize()
 }
@@ -11,7 +11,6 @@ fn tokenize(input: &str) -> Vec<TokenType> {
 struct Tokenizer<'a> {
     input: &'a str,
     input_chars: Enumerate<Chars<'a>>,
-    idx: u32,
 }
 
 impl<'a> Tokenizer<'a> {
@@ -19,18 +18,17 @@ impl<'a> Tokenizer<'a> {
         Self {
             input,
             input_chars: input.chars().enumerate(),
-            idx: 0,
         }
     }
 
-    fn tokenize(&mut self) -> Vec<TokenType> {
+    fn tokenize(&mut self) -> Vec<Token<()>> {
         use TokenType::*;
 
         let mut jump_stack = Vec::new();
 
-        let mut tokens = Vec::with_capacity(self.input.len());
+        let mut tokens: Vec<Token<()>> = Vec::with_capacity(self.input.len());
         while let Some((i, c)) = self.input_chars.next() {
-            let tok = match c {
+            let tok_type = match c {
                 '>' => Some(IncrDp),
 
                 '<' => Some(DecrDp),
@@ -44,22 +42,22 @@ impl<'a> Tokenizer<'a> {
                 ',' => Some(Input),
 
                 '[' => {
-                    jump_stack.push(i as u32);
+                    jump_stack.push(tokens.len() as u32);
                     // u32::MAX is used as a sentinel value
                     Some(JmpLeft(u32::MAX))
                 }
 
                 ']' => {
-                    let jmpl_addr = jump_stack
+                    let jmpl_token_idx = jump_stack
                         .pop()
                         .expect("Right bracket without matching LBracket");
 
                     // Assert that there is a matching uninitialized jumpleft
                     // before updating it's addres
-                    debug_assert_eq!(tokens[jmpl_addr as usize], JmpLeft(u32::MAX));
-                    tokens[jmpl_addr as usize] = JmpLeft(i as u32);
+                    debug_assert_eq!(tokens[jmpl_token_idx as usize].r#type, JmpLeft(u32::MAX));
+                    tokens[jmpl_token_idx as usize].r#type = JmpLeft(tokens.len() as u32);
 
-                    Some(JmpRight(jmpl_addr))
+                    Some(JmpRight(jmpl_token_idx))
                 }
                 _ => {
                     // Any other character is treated as a comment
@@ -67,122 +65,175 @@ impl<'a> Tokenizer<'a> {
                 }
             };
 
-            if let Some(t) = tok {
-                tokens.push(t);
+            if let Some(t_type) = tok_type {
+                tokens.push(Token { r#type: t_type, data: () });
             }
         }
+
+        assert!(jump_stack.is_empty());
 
         tokens
     }
 }
 
-// #[cfg(test)]
-// mod test {
-//     use crate::{TokenType, tokenizer::tokenize};
-//
-//     #[test]
-//
-//     fn single_incrdp() {
-//         use TokenType::*;
-//         let inp = ">";
-//         let tokens = tokenize(inp);
-//
-//         assert_eq!(tokens, vec![IncrDp]);
-//     }
-//
-//     #[test]
-//     fn single_decr_dp() {
-//         use TokenType::*;
-//         let inp = "<";
-//         let tokens = tokenize(inp);
-//         assert_eq!(tokens, vec![DecrDp]);
-//     }
-//
-//     #[test]
-//     fn single_incr_byte() {
-//         use TokenType::*;
-//         let inp = "+";
-//         let tokens = tokenize(inp);
-//         assert_eq!(tokens, vec![IncrByte]);
-//     }
-//
-//     #[test]
-//     fn single_decr_byte() {
-//         use TokenType::*;
-//         let inp = "-";
-//         let tokens = tokenize(inp);
-//         assert_eq!(tokens, vec![DecrByte]);
-//     }
-//
-//     #[test]
-//     fn single_output() {
-//         use TokenType::*;
-//         let inp = ".";
-//         let tokens = tokenize(inp);
-//         assert_eq!(tokens, vec![Output]);
-//     }
-//
-//     #[test]
-//     fn single_input() {
-//         use TokenType::*;
-//         let inp = ",";
-//         let tokens = tokenize(inp);
-//         assert_eq!(tokens, vec![Input]);
-//     }
-//
-//     #[test]
-//     fn single_jmp_left() {
-//         use TokenType::*;
-//         let inp = "[";
-//         let tokens = tokenize(inp);
-//         assert_eq!(tokens, vec![JmpLeft]);
-//     }
-//
-//     #[test]
-//     fn single_jmp_right() {
-//         use TokenType::*;
-//         let inp = "]";
-//         let tokens = tokenize(inp);
-//         assert_eq!(tokens, vec![JmpRight]);
-//     }
-//
-//     #[test]
-//     fn comments_are_ignored() {
-//         let inp = "><+-.,[]abcde";
-//         let tokens = tokenize(inp);
-//         use TokenType::*;
-//         assert_eq!(
-//             tokens,
-//             vec![
-//                 IncrDp, DecrDp, IncrByte, DecrByte, Output, Input, JmpLeft, JmpRight
-//             ]
-//         );
-//     }
-//
-//     #[test]
-//     fn mixed_commands() {
-//         let inp = "++[><].,-";
-//         let tokens = tokenize(inp);
-//         use TokenType::*;
-//         assert_eq!(
-//             tokens,
-//             vec![
-//                 IncrByte, IncrByte, JmpLeft, IncrDp, DecrDp, JmpRight, Output, Input, DecrByte
-//             ]
-//         );
-//     }
-//
-//     #[test]
-//     fn empty_input() {
-//         let inp = "";
-//         let tokens = tokenize(inp);
-//         assert!(tokens.is_empty());
-//     }
-//
-//     #[test]
-//     fn only_comments() {
-//         let inp = "hello world";
-//         let tokens = tokenize(inp);
-//         assert!(tokens.is_empty());
-//     }
-// }
+#[cfg(test)]
+mod test {
+    use crate::{Token, TokenType, tokenizer::tokenize};
+
+    #[test]
+    fn single_incrdp() {
+        use TokenType::*;
+        let inp = ">";
+        let tokens = tokenize(inp);
+
+        assert_eq!(tokens[0].r#type, IncrDp);
+    }
+
+    #[test]
+    fn single_decr_dp() {
+        use TokenType::*;
+        let inp = "<";
+        let tokens = tokenize(inp);
+        assert_eq!(tokens[0].r#type, DecrDp);
+    }
+
+    #[test]
+    fn single_incr_byte() {
+        use TokenType::*;
+        let inp = "+";
+        let tokens = tokenize(inp);
+        assert_eq!(tokens[0].r#type, IncrByte);
+    }
+
+    #[test]
+    fn single_decr_byte() {
+        use TokenType::*;
+        let inp = "-";
+        let tokens = tokenize(inp);
+        assert_eq!(tokens[0].r#type, DecrByte);
+    }
+
+    #[test]
+    fn single_output() {
+        use TokenType::*;
+        let inp = ".";
+        let tokens = tokenize(inp);
+        assert_eq!(tokens[0].r#type, Output);
+    }
+
+    #[test]
+    fn single_input() {
+        use TokenType::*;
+        let inp = ",";
+        let tokens = tokenize(inp);
+        assert_eq!(tokens[0].r#type, Input);
+    }
+
+    #[test]
+    fn comments_are_ignored() {
+        let inp = "><+-.,[]abcde";
+        let tokens = tokenize(inp);
+        use TokenType::*;
+        assert_eq!(
+            tokens.iter().map(|t| t.r#type).collect::<Vec<TokenType>>(),
+            vec![
+                IncrDp,
+                DecrDp,
+                IncrByte,
+                DecrByte,
+                Output,
+                Input,
+                JmpLeft(7),
+                JmpRight(6)
+            ]
+        );
+    }
+
+    #[test]
+    fn mixed_commands() {
+        let inp = "++[><].,-";
+        let tokens = tokenize(inp);
+        use TokenType::*;
+        assert_eq!(
+            tokens.iter().map(|t| t.r#type).collect::<Vec<TokenType>>(),
+            vec![
+                IncrByte,
+                IncrByte,
+                JmpLeft(5),
+                IncrDp,
+                DecrDp,
+                JmpRight(2),
+                Output,
+                Input,
+                DecrByte
+            ]
+        );
+    }
+
+    #[test]
+    fn empty_input() {
+        let inp = "";
+        let tokens = tokenize(inp);
+        assert!(tokens.is_empty());
+    }
+
+    #[test]
+    fn only_comments() {
+        let inp = "hello world";
+        let tokens = tokenize(inp);
+        assert!(tokens.is_empty());
+    }
+
+    #[test]
+    fn simple_loop() {
+        let inp = "[]";
+        let tokens = tokenize(inp);
+        use TokenType::*;
+        assert_eq!(tokens.iter().map(|t| t.r#type).collect::<Vec<TokenType>>(), vec![JmpLeft(1), JmpRight(0)]);
+    }
+
+    #[test]
+    fn nested_loops() {
+        let inp = "[[]]";
+        let tokens = tokenize(inp);
+        use TokenType::*;
+        assert_eq!(
+            tokens.iter().map(|t| t.r#type).collect::<Vec<TokenType>>(),
+            vec![JmpLeft(3), JmpLeft(2), JmpRight(1), JmpRight(0)]
+        );
+    }
+
+    #[test]
+    fn complex_loop_structure() {
+        let inp = "[[>+<]-]";
+        let tokens = tokenize(inp);
+        use TokenType::*;
+        assert_eq!(
+            tokens.iter().map(|t| t.r#type).collect::<Vec<TokenType>>(),
+            vec![
+                JmpLeft(7),
+                JmpLeft(5),
+                IncrDp,
+                IncrByte,
+                DecrDp,
+                JmpRight(1),
+                DecrByte,
+                JmpRight(0)
+            ]
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "Right bracket without matching LBracket")]
+    fn unmatched_right_bracket() {
+        let inp = "]";
+        tokenize(inp);
+    }
+
+    #[test]
+    #[should_panic]
+    fn unmatched_left_bracket() {
+        let inp = "[";
+        tokenize(inp);
+    }
+}
