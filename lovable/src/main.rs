812281099333df55a7ad82098d5a571e55cb259b
Nathan Kamenchu <kamenchunathan@gmail.com>
2025-09-01 08:02:01 +0300

    feat: Create window with desired properties and test out pngs

--- lovable/src/main.rs
+++ lovable/src/main.rs
@@ -2,36 +2,40 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                name: Some("Lovable".to_string()),
+                decorations: false,
+                transparent: true,
+                composite_alpha_mode: bevy::window::CompositeAlphaMode::Opaque,
+                window_level: bevy::window::WindowLevel::AlwaysOnTop,
+                skip_taskbar: true,
+                movable_by_window_background: true,
+                position: WindowPosition::At(IVec2::splat(100)),
+                ..default()
+            }),
+            ..default()
+        }))
         .add_systems(Startup, setup)
+        .add_systems(Update, (update, quit_on_esc))
         .run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2d);
 
-    let sprite_handle = asset_server.load("Sprite-0001");
-
-    commands.spawn((
-        Sprite::from_image(sprite_handle.clone()),
-        Transform::from_xyz(-100.0, 0.0, 0.0),
-    ));
-    commands.spawn((
-        Sprite {
-            image: sprite_handle.clone(),
-            // Alpha channel of the color controls transparency.
-            color: Color::srgba(0.0, 0.0, 1.0, 0.7),
-            ..default()
-        },
-        Transform::from_xyz(0.0, 0.0, 0.1),
-    ));
-    commands.spawn((
-        Sprite {
-            image: sprite_handle,
-            color: Color::srgba(0.0, 1.0, 0.0, 0.3),
-            ..default()
-        },
-        Transform::from_xyz(100.0, 0.0, 0.2),
-    ));
+    let sprite_handle = asset_server.load("Sprite-0001.png");
+    commands.spawn((Sprite::from_image(sprite_handle.clone()),));
 }
 
+fn update(mut query: Query<&mut Transform, With<Sprite>>, time: Res<Time>) {
+    for mut transform in &mut query {
+        transform.translation.y += time.delta_secs() * 20.0;
+    }
+}
+
+fn quit_on_esc(keys: ResMut<ButtonInput<KeyCode>>, mut app_exit_events: EventWriter<AppExit>) {
+    if keys.just_pressed(KeyCode::Escape) | keys.just_pressed(KeyCode::KeyQ) {
+        app_exit_events.write(AppExit::Success);
+    }
+}
