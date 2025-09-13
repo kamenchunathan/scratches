09c6b681ee6eabd19e35d4cc9953cb417ee4a0ce
Nathan Kamenchu <kamenchunathan@gmail.com>
2025-09-13 14:55:12 +0300

    feat: Implement simple allocators

--- remember/README.md
+++ remember/README.md
@@ -1,3 +1,7 @@
 # Remember
 
 A memory allocator lab in C++
+
+# References 
+- [](https://johnfarrier.com/custom-allocators-in-c-high-performance-memory-management/)
+- [](https://en.cppreference.com/w/cpp/memory/allocator/)
--- remember/labs/wrapper.cpp
+++ remember/labs/wrapper.cpp
@@ -1,6 +1,23 @@
+#include "../src/include/lib.hpp"
 #include <cstdio>
+#include <ranges>
+#include <vector>
 
 int main() {
-  std::printf("Hello world");
+  Mallocator<int> alloc;
+  {
+    std::vector<int, Mallocator<int>> vec(alloc);
+
+    for (auto i : std::ranges::views::iota(0, 64)) {
+      vec.push_back(i);
+    }
+
+    for (auto i : vec) {
+      printf("%i ", i);
+    }
+  }
+
+  // AllocationTracker::instance().print_summary();
+
   return 0;
 }
