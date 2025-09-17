581bfefbee1f2f0a90b21a67033ce20c9d1ae8f4
Nathan Kamenchu <kamenchunathan@gmail.com>
2025-09-17 22:27:12 +0300

    feat: Add renderer

---
+++ hopper/engine/include/color.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <cmath>
+#include <cstdint>
+#include <stdfloat>
+
+namespace core {
+
+struct ColorRGB8 {
+    std::uint8_t r, g, b;
+
+    // Editor highlighting
+    static ColorRGB8 rgb(
+        std::uint8_t r,
+        std::uint8_t g,
+        std::uint8_t b
+
+    ) {
+        return ColorRGB8 { .r = r, .g = g, .b = b };
+    }
+};
+
+struct ColorRGBA8 {
+    std::uint8_t r, g, b, a;
+
+    static ColorRGBA8 rgba(
+        std::uint8_t r,
+        std::uint8_t g,
+        std::uint8_t b,
+        std::uint8_t a
+
+    ) {
+        return ColorRGBA8 { .r = r, .g = g, .b = b, .a = a };
+    }
+};
+
+struct ColorRGBA32F {
+    std::float32_t r, g, b, a;
+
+    static ColorRGBA32F rgba(
+        std::float32_t r,
+        std::float32_t g,
+        std::float32_t b,
+        std::float32_t a
+
+    ) {
+        return ColorRGBA32F { .r = r, .g = g, .b = b, .a = a };
+    }
+};
+
+} // namespace core
--- hopper/hopper/client/main.cpp
+++ hopper/hopper/client/main.cpp
@@ -1,2 +1,32 @@
+#include "color.hpp"
+#include "renderer.hpp"
+
+void draw_filled_rect(
+    renderer::OutputBuffers& buffer,
+    int x,
+    int y,
+    int w,
+    int h,
+    core::ColorRGBA32F color
+) {
+    for (int i = x; i < x + w; ++i) {
+        for (int j = y; j < y + h; ++j) {
+            if (i >= 0 && i < buffer.pixel_width() && j >= 0 && j < buffer.pixel_height()) {
+                buffer.pixel_buf[j * buffer.pixel_width() + i] = color;
+            }
+        }
+    }
+}
+
 int main() {
+    renderer::Renderer app_renderer(160, 90);
+
+    app_renderer.front_buffer.clear({ 0.8f, 0.3f, 0.0f, 1.0f });
+
+    draw_filled_rect(app_renderer.front_buffer, 10, 10, 20, 20, { 0.0f, 1.0f, 0.0f, 1.0f });
+    draw_filled_rect(app_renderer.front_buffer, 40, 10, 20, 20, { 0.0f, 0.0f, 1.0f, 1.0f });
+    draw_filled_rect(app_renderer.front_buffer, 10, 40, 20, 20, { 1.0f, 0.0f, 0.0f, 1.0f });
+    draw_filled_rect(app_renderer.front_buffer, 40, 40, 20, 20, { 1.0f, 1.0f, 0.0f, 1.0f });
+
+    app_renderer.present();
 }
