e9adf9345f9fb253f6c43e18bd4f5c2d8316dc2a
Nathan <kamenchunathan@gmail.com>
2025-11-01 07:05:03 +0000

    refactor: Update the browser example to showcase mutliplatfrom code
    Runs on both the browser and terminal

--- hopper/labs/browsertest.cpp
+++ hopper/labs/browsertest.cpp
@@ -7,10 +7,11 @@
 #include "time.hpp"
 
 #if PLATFORM_WASM
-    #include "web/layer.hpp"
-    #include "web/presenter.hpp"
     #include <emscripten.h>
     #include <emscripten/html5.h>
+
+    #include "web/layer.hpp"
+    #include "web/presenter.hpp"
 #else
     #include "term/layer.hpp"
     #include "term/presenter.hpp"
@@ -36,11 +37,11 @@ public:
             std::make_unique<web::BrowserPresenter>()
         );
 #else
-        auto renderer = std::make_unique<renderer::Renderer>(
-            width,
-            height,
-            std::make_unique<term::Presenter>()
-        );
+        auto opt_presenter = app->world.remove_resource<std::unique_ptr<TerminalPresenter>>();
+        if (!opt_presenter.has_value())
+            return;
+        auto renderer =
+            std::make_unique<renderer::Renderer>(width, height, std::move(opt_presenter.value()));
 #endif
 
         app->world.insert_resource(std::move(renderer));
@@ -48,39 +49,42 @@ public:
 };
 
 void animation_system(ecs::World& world) {
-    auto* time = world.get_resource<core::Time>();
-    if (!time)
+    auto opt_time = world.get_resource<core::Time>();
+    if (!opt_time)
         return;
+    auto& time = opt_time->get();
 
-    auto* animation = world.get_resource<Animation>();
-    if (!animation)
+    auto opt_animation = world.get_resource<Animation>();
+    if (!opt_animation)
         return;
+    auto& animation = opt_animation->get();
 
-    animation->time_since_last_frame += time->delta_seconds;
-    if (animation->time_since_last_frame >= animation->frame_duration) {
-        animation->time_since_last_frame -= animation->frame_duration;
-        animation->current_frame = (animation->current_frame + 1) % animation->frames.size();
+    animation.time_since_last_frame += time.delta_seconds;
+    if (animation.time_since_last_frame >= animation.frame_duration) {
+        animation.time_since_last_frame -= animation.frame_duration;
+        animation.current_frame = (animation.current_frame + 1) % animation.frames.size();
     }
 }
 
 void render_system(ecs::World& world) {
-    auto* renderer_ptr = world.get_resource<std::unique_ptr<renderer::Renderer>>();
-    if (!renderer_ptr || !*renderer_ptr)
+    auto opt_renderer_ptr = world.get_resource<std::unique_ptr<renderer::Renderer>>();
+    if (!opt_renderer_ptr)
         return;
-    renderer::Renderer& renderer = **renderer_ptr;
+    auto& renderer = opt_renderer_ptr->get();
 
-    auto* animation = world.get_resource<Animation>();
-    if (!animation)
+    auto opt_animation = world.get_resource<Animation>();
+    if (!opt_animation)
         return;
+    auto& animation = opt_animation->get();
 
     renderer::FrameBuffer<renderer::Attachment<renderer::CharacterPixel>> render_target =
-        renderer.render_target();
+        renderer->render_target();
     renderer::TextureHandle<renderer::CharacterPixel> render_texture_handle =
         std::get<0>(render_target.attachments).view.texture;
 
     renderer::Texture2D<renderer::CharacterPixel>* render_texture = nullptr;
     if (auto maybe_render_texture =
-            renderer.resource_registry.get_texture_mut(render_texture_handle);
+            renderer->resource_registry.get_texture_mut(render_texture_handle);
         maybe_render_texture.has_value())
     {
         render_texture = maybe_render_texture.value();
@@ -96,7 +100,7 @@ void render_system(ecs::World& world) {
     );
 
     // Draw animation
-    const std::string& frame = animation->frames[animation->current_frame];
+    const std::string& frame = animation.frames[animation.current_frame];
     if (!frame.empty()) {
         int x = render_texture->width() / 2;
         int y = render_texture->height() / 2;
@@ -107,7 +111,7 @@ void render_system(ecs::World& world) {
         };
     }
 
-    renderer.render_frame();
+    renderer->render_frame();
 }
 
 int main() {
@@ -118,7 +122,8 @@ int main() {
 #if PLATFORM_WASM
     app->add_layer(BrowserLayer {});
 #else
-    app->add_layer(TerminalLayer {});
+    auto term_layer = TerminalLayer();
+    app->add_layer(term_layer);
 #endif
     app->add_layer(RendererLayer {});
 
