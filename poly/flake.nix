86a0892f4215ff3b99446670e672a096714ee679
Nathan Kamenchu <kamenchunathan@gmail.com>
2025-11-04 15:41:47 +0300

    init: Initial commit

---
+++ poly/flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "A Nix-flake-based C/C++ development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+
+  outputs =
+    { self, ... }@inputs:
+
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default =
+            pkgs.mkShell.override
+              {
+                # Override stdenv in order to change compiler:
+                # stdenv = pkgs.clangStdenv;
+              }
+              {
+                packages =
+                  with pkgs;
+                  [
+                    meson
+                    gcc
+                    libgcc
+                    clang
+                  ];
+                env = {
+                  LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+                  CPATH = "${pkgs.clang}/include";
+                };
+              };
+        }
+      );
+    };
+}
