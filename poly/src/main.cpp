1bf661a1507a02df5e3cdfe083f58fccf0726b26
Nathan Kamenchu <kamenchunathan@gmail.com>
2025-11-16 11:01:05 +0300

    feat: working version

--- poly/src/main.cpp
+++ poly/src/main.cpp
@@ -1,5 +1,14 @@
+#include <cstdint>
+#include <meta>
 #include <print>
 
+struct Point {
+    std::uint32_t x, y;
+};
+
 int main() {
-  std::println("Hello world");
+    constexpr auto point_refl = ^^Point;
+    constexpr auto type_name = std::meta::is_namespace_member(point_refl);
+    std::println("type name {}", type_name);
+    return 0;
 }
